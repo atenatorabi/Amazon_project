f5cbcae189ec0f2a26380899a2b0f12f1ebe757a
Atena <atenatorabi774v@gmail.com>
2025-06-02 12:28:17 -0700

    third edit

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'home_app',
     'product_app',
     'news_app',
+    'user_app',
 ]
 
 MIDDLEWARE = [
@@ -72,6 +73,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+AUTH_USER_MODEL = 'user_app.User'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
--- product_app/views.py
+++ product_app/views.py
@@ -40,4 +40,3 @@ def product_categories(request , id):
 
 
 
-
